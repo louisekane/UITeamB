5e2f2def495c87d01b0f2ef4d989978f0a43b31c
Maciej Kaczor <maciej.kaczor@kainos.com>
2022-07-06 15:09:25 +0200

    fix small issues

--- app/jobRoleData.js
+++ app/jobRoleData.js
@@ -1,13 +1,11 @@
 const axios = require('axios')
 
 exports.getJobRoles = async () => { 
-    let jobRoles = []  
       try {  
         const jobRoleResponse = await axios.get('http://localhost:8080/api/job-roles')
-        jobRoles = jobRoleResponse.data
+        return jobRoleResponse.data
       } catch (e) {
          return new Error('Could not get job roles')
 
       }
-      return jobRoles;
   }
