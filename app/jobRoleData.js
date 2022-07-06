3d0f4a619aa847ce103af4af3177c715f0d16137
Maciej Kaczor <maciej.kaczor@kainos.com>
2022-07-06 10:32:41 +0200

    add job roles UI table

---
+++ app/jobRoleData.js
@@ -0,0 +1,13 @@
+const axios = require('axios')
+
+exports.getJobRole = async () => { 
+    let jobRoles = []  
+      try {  
+        const jobRoleResponse = await axios.get('http://localhost:8080/api/job-roles')
+        jobRoles = jobRoleResponse.data
+      } catch (e) {
+         return new Error('Could not get job roles')
+
+      }
+      return jobRoles;
+  }
