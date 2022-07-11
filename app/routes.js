f2c4409eb36bda5c78c325e2a5652879c40d71de
Maciej Kaczor <maciej.kaczor@kainos.com>
2022-07-11 15:21:43 +0200

    remove unnecessary content

--- app/routes.js
+++ app/routes.js
@@ -1,15 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const jobRoleData = require('./jobRoleData.js')
-
-router.get('/jobRoles', async (req, res) => {
-
-    res.render('jobRolesView', {
-      jobRoles: await jobRoleData.getJobRoles()
-    }
-    ); 
-  
-  });
 
 router.get('/home', (req, res) => {
   res.render('homeView');
