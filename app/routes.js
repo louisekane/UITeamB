2b02dab5a6238cbe66bfde3c1ad08ae0d64a6809
Louise Kane <louise.kane@kainos.com>
2022-07-12 12:13:36 +0100

    route

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,8 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+router.post('/registeruser', async (req, res) => {
+    res.render('/registeruser' { } )
+    
+    });
