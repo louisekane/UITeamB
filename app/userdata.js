c1b00b8552b4b0b874a609f28cf0158817ce9a10
Adam Elliott <adam.elliott@kainos.com>
2022-07-18 09:55:46 +0100

    Address PR changes

--- app/userdata.js
+++ app/userdata.js
@@ -3,31 +3,13 @@ const axios = require('axios');
 const dbconfig = require('../dbconfig.json');
 const util = require('util')
 
-const db = wrapDB(dbconfig)
-
-
-
-function wrapDB(dbconfig) {
-    const pool = mysql.createPool(dbconfig)
-    return {
-      query(sql, args) {
-        console.log("in query in wrapper")
-        return util.promisify(pool.query)
-          .call(pool, sql, args)
-      },
-      release() {
-        return util.promisify(pool.releaseConnection)
-          .call(pool)
-      }
-    }
+// Check this method 
+exports.createUser = async (newUser) => {
+  try {
+    const usersResponse = await axios.post('http://localhost:8080/api/registeruser', { email: newUser.email, password: newUser.password, role: newUser.role })
+    return usersResponse
+  } catch (e) {
+    throw 'An error has occured, please try again later.';
   }
-  // Check this method 
-  exports.createUser = async (newUser) => {
-    try {
-      const usersResponse = await axios.post('http://localhost:8080/api/registeruser', { email: newUser.email, password: newUser.password, role: newUser.role })
-      return usersResponse
-    } catch (e) {
-      throw 'An error has occured, please try again later.';
-    }
-  }
+}
 
